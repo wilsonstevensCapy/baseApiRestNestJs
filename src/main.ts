a7128c7b0c24254ba8603b42157bd828d94175a3
wilsonstevensCapy <stevenscard06@gmail.com>
2022-06-29 18:26:16 -0500

    api res con nestjs base

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Api Documentation')
+    .setDescription('Esto es una prueba')
+    .setVersion('1.0')
+    .addTag('items') //aqui va los demas o logica de negocio
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
